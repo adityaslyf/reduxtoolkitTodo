75ad2f947fc6607512baa2557613ab6a5f7700cd
adityaslyf <aditya.varshneymail@gmail.com>
2024-10-03 00:15:19 +0530

    creating slices for adding todo and removing

--- app/feature/Todo/todoSlice.ts
+++ app/feature/Todo/todoSlice.ts
@@ -11,6 +11,25 @@ const initialState = {
     error: null,
 };
 
+export const todoSlice = createSlice({
+    name: "todo",
+    initialState,
+    reducers: {
+        addTodo: (state, action) => {
+            state.todos.push({
+                id: nanoid(),
+                text: action.payload,
+            });
+        },
+        removeTodo: (state, action) => {
+            state.todos = state.todos.filter((todo) => todo.id !== action.payload);
+        },
+    },
+});
+
+export const { addTodo, removeTodo } = todoSlice.actions;
+
+
 
 
 
